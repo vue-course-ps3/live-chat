91b73e499582ddfbe5d53992390b36620582307d
maxim-peeters <maximpeeters38@gmail.com>
2025-01-08 10:20:08 +0100

    lesson 102

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAkRFS76DIWr7mJxkuQeeQsc8t-kaa0B4",
+  authDomain: "udemy-vue-firebase-sites-e859c.firebaseapp.com",
+  projectId: "udemy-vue-firebase-sites-e859c",
+  storageBucket: "udemy-vue-firebase-sites-e859c.firebasestorage.app",
+  messagingSenderId: "31456600145",
+  appId: "1:31456600145:web:e210d29c8f26f542bc1870"
+};
+
+// init Firebase
+firebase.initializeApp(firebaseConfig)
+
+// init Firestore
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
---
+++ src/views/WelcomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container welcome">
+    <p>Welcome</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  .welcome{
+    text-align: center;
+    padding: 20px 0;
+
+  }
+</style>
